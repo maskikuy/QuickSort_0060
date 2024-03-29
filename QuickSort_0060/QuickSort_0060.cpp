4851c139462617d5f1a551c7f20f267435e48c99
maskikuy <kproject305@gmail.com>
2024-03-29 15:05:23 +0700

    swap the element ke index x

--- QuickSort_0060/QuickSort_0060.cpp
+++ QuickSort_0060/QuickSort_0060.cpp
@@ -23,5 +23,14 @@ void input() {
     cout << "\nEnter Array Element" << endl;
     cout << "\n-------------------" << endl;
 
- 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+//swaps the element at index x with the element at index y
+void swap(int x, int y)
+{
+
 }
