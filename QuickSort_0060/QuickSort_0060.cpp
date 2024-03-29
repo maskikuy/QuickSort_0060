56b54109080df506c0d2640fd61d7b649699387a
maskikuy <kproject305@gmail.com>
2024-03-29 14:55:12 +0700

    input integer array

--- QuickSort_0060/QuickSort_0060.cpp
+++ QuickSort_0060/QuickSort_0060.cpp
@@ -3,23 +3,3 @@ using namespace std;
 
 //array of integers to hold values
 int arr[20];
-int cmp_count = 0; // number of comparasion
-int mov_count = 0; // number of data movement
-int n;
-
-void input() {
-    while (true)
-    {
-        cout << "Masukan panjang element array: ";
-        cin >> n;
-
-        if (n <= 20)
-            break;
-        else
-            cout << "\nMaksimum panjang array adalah 20" << endl;
-    }
-}
-int main()
-{
-    std::cout << "Hello World!\n";
-}
